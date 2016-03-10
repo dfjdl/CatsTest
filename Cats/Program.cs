e2469df110fc65d4a8ab665a5f82e1d5c5a9f55e
dfjdl <dfjdl@mailinator.com>
2016-03-10 23:20:06 +1100

    Initial commit
    Initail

---
+++ Cats/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace Cats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = client.GetAsync("http://agl-developer-test.azurewebsites.net/people.json").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var json = response.Content.ReadAsStringAsync().Result;
+                var values = JArray.Parse(json);
+
+                foreach (var group in values.GroupBy(people => (string) people["gender"]))
+                {
+                    Console.WriteLine(group.Key);
+                    Console.WriteLine("============");
+                    foreach (
+                        var catName in
+                            group.SelectMany(people => people["pets"])
+                                .Where(pet => (string) pet["type"] == "Cat")
+                                .Select(cat => (string) cat["name"])
+                                .OrderBy(name => name))
+                    {
+                        Console.WriteLine(catName);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
